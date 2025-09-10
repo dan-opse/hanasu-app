52af8d5782b94145c089bcf6d48fd9e46aee1362
ccdeeni <113562568+dan-opse@users.noreply.github.com>
2025-09-10 14:12:34 -0400

    Nothing done; starting to add analysis logic from analysis.js into analyze route

--- app/api/analyze/route.js
+++ app/api/analyze/route.js
@@ -3,3 +3,31 @@
 // Purpose: Analyze transcribed text -> provide feedback
 // Input: Transcribed text (transcription route)
 // Output: Analysis as JSON file
+
+import { NextResponse } from "next/server";
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import parse_json from "./parse_json.js";
+
+const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
+const llm = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+// Stopping here August 14, 11:13 AM:
+// [ ] - need to add more input variables: inputText, language, model
+// [ ] - combine analysis.js with this file
+
+export async function POST(request) {
+  console.log("\n--- Running Analysis ---\n");
+
+  try {
+    const result = await request;
+  } catch (e) {
+    console.log(e);
+    return NextResponse.json(
+      {
+        error: e.message,
+        success: false,
+      },
+      { status: 500 }
+    );
+  }
+}
